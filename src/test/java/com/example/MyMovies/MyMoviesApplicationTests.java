6e560f562d5c85938c93a4b8fde76690065c3339
pratibha <pratibha.maind@thoughtworks.com>
2025-05-18 12:54:25 +0530

    Initial commit

---
+++ src/test/java/com/example/MyMovies/MyMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyMovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
