f3bb09a15dad38da8e991e449b5b7d4b27f39068
pratibha <pratibha.maind@thoughtworks.com>
2025-05-18 12:59:36 +0530

    Add working setup for API

---
+++ src/main/java/com/example/MyMovies/model/Movie.java
@@ -0,0 +1,57 @@
+package com.example.MyMovies.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private double rating;
+    private String genre;
+
+    public Movie(String name, double rating, String genre){
+//        this.id = (int)(Math.random()* 100);
+        this.name = name;
+        this.rating = rating;
+        this.genre = genre;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
